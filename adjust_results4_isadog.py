0ce08c53ffa3a8837859a89cbb33dc809fa530a0
Ariel-Chole <168365373+Ariel-Chole@users.noreply.github.com>
2024-07-18 12:27:48 +0630

    Update adjust_results4_isadog.py

--- adjust_results4_isadog.py
+++ adjust_results4_isadog.py
@@ -4,7 +4,7 @@
 #                                                                             
 # PROGRAMMER: Chit Zin Win
 # DATE CREATED: 15-7-2024                               
-# REVISED DATE: 
+# REVISED DATE: 18-7-2024
 # PURPOSE: Create a function adjust_results4_isadog that adjusts the results 
 #          dictionary to indicate whether or not the pet image label is of-a-dog, 
 #          and to indicate whether or not the classifier image label is of-a-dog.
